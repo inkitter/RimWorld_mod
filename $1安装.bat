8930869b18cfacb8664c2016a446284801f06fa6
Inkit <inkitter@gmail.com>
2019-12-09 07:44:03 +0800

    Init

--- $1安装.bat
+++ $1安装.bat
@@ -1,14 +1,12 @@
 @echo off
 chcp 65001
-FOR /F "tokens=3* delims= " %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Personal"') do (set mydoc=%%a)
+set mydoc="D:\SteamLibrary\steamapps\common\RimWorld\Mods"
 cd /d %~dp0
-echo ----将在%mydoc%\Paradox Interactive\Hearts of Iron IV\mod\安装mod----
+echo ----将在 %mydoc% 安装InkitPack----
 pause
 @echo on
 
-md "%mydoc%\Paradox Interactive\Hearts of Iron IV\mod"
-mklink /j "%mydoc%\Paradox Interactive\Hearts of Iron IV\mod\_Inkitmod" _Inkitmod
-copy *.mod "%mydoc%\Paradox Interactive\Hearts of Iron IV\mod" /y
+mklink /j "%mydoc%" InkitPack
 
 @echo ----链接已建立：mod文件夹与本文件夹的链接，删除本文件夹内容会使mod失效----
 @echo ----若无效请查看是否有错误提示----
