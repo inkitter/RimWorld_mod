9bba412b1945a53faed319e6f88178b1846b494b
Inkit <inkitter@gmail.com>
2020-02-28 09:56:59 +0800

    Update

--- $1安装.bat
+++ $1安装.bat
@@ -6,8 +6,8 @@ echo ----将在 %mydoc% 安装InkitPack----
 pause
 @echo on
 
-mklink /j "%mydoc%\InkitPack" InkitPack
-mklink /j "%mydoc%\InkitPackCE" InkitPackCE
+REM mklink /j "%mydoc%\Inkit_Dlls" Inkit_Dlls
+mklink /j "%mydoc%\Inkit_Godthings" Inkit_Godthings
 
 @echo ----链接已建立：mod文件夹与本文件夹的链接，删除本文件夹内容会使mod失效----
 @echo ----若无效请查看是否有错误提示----
